37a5238d9cebc2e96805abd4457943bfe7b6f5d3
AlexSumakAl <AlexSumak@proton.me>
2022-07-28 23:16:58 +0300

    add html in messages

--- main.py
+++ main.py
@@ -10,10 +10,12 @@ logger = telebot.logger
 logger.setLevel(logging.DEBUG)
 
 
-@bot.message_handler(commands=["start"])
+@bot.message_handler(commands=["start", 'control'])
 def start(message):
     username = message.from_user.username
-    bot.reply_to(message, f'Hello, {username}')
+    # bot.reply_to(message, f'Hello, {username}')
+    mess = f'Привет, <b><u>{message.from_user.username}</u></b>'
+    bot.send_message(message.chat.id, mess, parse_mode='html')
 
 
 @server.route(f"/{BOT_TOKEN}", methods=["POST"])
